b0562d7f228236643cb549b162f70d9a42fac3b7
Zanetti <michele.zanetti-4@hpc-head-n2.unitn.it>
2025-11-19 14:56:37 +0100

    timingsBenchmark executed

---
+++ project/matrix.h
@@ -0,0 +1,23 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <string>
+
+class matrix {
+    int numRows;
+    int numCols;
+
+    int* aRows;
+    int* aCols;
+    double* values;
+
+    double* productArr;
+public:
+    matrix(const std::string fileName);
+    ~matrix();
+
+    void sequentialProduct(double* &resultArray, int &arrLength);
+    void parallelProduct(double* &resultArray, int &arrLength, int numOfThreads);
+};
+
+#endif
