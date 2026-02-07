b17ac5509cc0b7fafc9e5bdc8556431ef7594322
Michele Zanetti <michele.zanetti-4@studenti.unitn.it>
2026-02-07 15:43:21 +0100

    continued main, made first script

--- Deliverable2/src/matrix.cpp
+++ Deliverable2/src/matrix.cpp
@@ -1,11 +1,12 @@
 #include "matrix.h"
 
 #include <iostream>
+#include <string>
 
 matrix::matrix(int localNumRows, int localNumValues):numRows(localNumRows), numValues(localNumValues) {
     aRows = new int[numRows + 1];
     aCols = new int[numValues];
-    values = new double[numValues];
+    vals = new double[numValues];
 
     std::fill(aRows, aRows + numRows + 1, 0);
 }
@@ -17,20 +18,33 @@ matrix::~matrix() {
 }
 
 void matrix::coo_to_csr(int* rows, int* cols, double* values) {
-	for(int i = 0;i < numRows + 1;i++)
-		aRows[i]++;
+	for(int i = 0;i < numValues + 1;i++) {
+		aRows[rows[i] + 1]++;
+		aCols[i] = cols[i];
+		vals[i] = values[i];
+	}
 		
-	for(int i = 0;i < numRows;i++)
+	for(int i = 1;i < numRows;i++)
 		aRows[i + 1] += aRows[i];
 }
 
 void matrix::print(int* rows, int* cols, double* values, int rank) {
-	std::cout << "Process " << rank << ":" << std::endl;
-	for(int i = 0;i < numRows + 1;i++)
-		std::cout << rows[i] << " ";
-		
-	std::cout << std::endl;	
+	std::string temp = "|";
+
+	for(int i = 0;i < numValues;i++)
+    	temp = temp + " " + std::to_string(rows[i]) + " - " + std::to_string(cols[i]) + " - " + std::to_string(values[i]) + " |";
+  
+	temp = temp + "\n";
+	std::cout << temp;
 }
 
-void matrix::spmv() {
+void matrix::spmv(double* x, double* y) {
+	for(int i = 0;i < numRows;i++) {
+        int startIndex = aRows[i];
+        int endIndex = aRows[i + 1];
+        
+        for(int j = startIndex;j < endIndex;j++) {
+            resultArr[i] += values[j] * productArr[aCols[j]];
+        }
+    }
 }
